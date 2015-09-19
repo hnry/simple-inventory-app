849705eb76fb35639bd7450957e16772d468455a
hc <henycr@gmail.com>
2015-09-19 19:20:08 -0400

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 500, height: 400});
+
+  var firsttime = false;
+  // TODO check if first time
+  var loadFile = '/first_html.html';
+  if (!firsttime) loadFile = '/index.html'; 
+  mainWindow.loadUrl('file://' + __dirname + loadFile);
+
+  // Open the DevTools.
+  mainWindow.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
